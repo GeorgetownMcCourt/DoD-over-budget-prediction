0767350424e6a8191a33e16be0b94f126e05417e
tythompson2 <tythompson1948@gmail.com>
2017-05-01 20:43:54 -0400

    Add files via upload

---
+++ Defense Contract Prediction (Tommie's Edits) 2.R
@@ -0,0 +1,190 @@
+#Prediction
+
+rm(list=ls(all=TRUE))
+install.packages("randomFOrest")
+install.packages("dplyr")
+library(randomForest)
+library(dplyr)
+
+setwd("C:/Users/bruis/OneDrive/Documents/Schoolwork/Intro to Data Science/Final Project")
+
+full.data <- read.csv("https://raw.githubusercontent.com/lipseylc/DoD-over-budget-prediction/master/data_for_test.csv")
+
+#Out of curiousity, what are we looking at here?
+#full.data as loaded is 100,000 observations, but that includes a lot of contracts that are still in progress
+#These lines estimate how many meet key conditions for our analysis
+nrow(full.data[full.data$IsClosed == "Closed",]) # 2341 contracts are closed out
+nrow(full.data[full.data$Term == "Terminated",]) # 853 of them were terminated
+nrow(full.data[full.data$Term == "Terminated" & full.data$IsClosed == "Closed",]) # Al 853 terminated were marked as closed (phew)
+
+#filter full data down to only those contracts that have been completed/closed
+full.data <- full.data[full.data$IsClosed == "Closed",]
+
+#remove all NAs
+complete.data <- full.data[complete.cases(full.data),] #~300 cases lost
+
+####
+#### Methods ####
+####
+
+
+#Mean
+meanf1 <- function(actual, predicted){
+  #Mean F1 score function
+  #actual = a vector of actual labels
+  #predicted = predicted labels
+  
+  classes <- unique(actual)
+  results <- data.frame()
+  for(k in classes){
+    results <- rbind(results, 
+                     data.frame(class.name = k,
+                                weight = sum(actual == k)/length(actual),
+                                precision = sum(predicted == k & actual == k)/sum(predicted == k), 
+                                recall = sum(predicted == k & actual == k)/sum(actual == k)))
+  }
+  results$score <- results$weight * 2 * (results$precision * results$recall) / (results$precision + results$recall) 
+  return(sum(results$score))
+}
+
+#Function to shuffle data and cut into folds
+#Copied from version emailed out to class after Homework 2
+kfolds.index <- function(n, k, random = TRUE){
+  # Returns a vector of labels for each of k-folds. 
+  # Useful for setting up k-folds cross validation
+  #
+  # Args:
+  #       n: data size
+  #       k: k-folds
+  #       random: whether folds should be sequential or randomized (default)
+  #
+  # Returns:
+  #       Vector of numeric labels
+  
+  #create row index
+  row.id <- 1:n
+  
+  #Decide splits
+  break.id <- cut(row.id, breaks = k, labels = FALSE)
+  
+  #Randomize
+  if(random == TRUE){
+    row.id <- row.id[sample(row.id, replace = FALSE)]
+  }
+  
+  #Package up
+  out <- data.frame(row.id = row.id, folds = break.id)
+  out <- out[order(out$row.id), ]
+  return(out[,2])
+}  
+
+#Simple predict function 
+pred.check <- function(x,y) { 
+  #"x" is the partition for validation and "y" is the model 
+  #so I only need to modify one model when checking 
+  pred <- predict(y, x, type="class")
+  print(meanf1(x$Term, pred)) 
+}
+
+
+####
+#### Preliminary Analysis section ####
+####
+
+
+#Recode Contract ID as a factor
+full.data$CSIScontractID <- as.factor(full.data$CSIScontractID) #why do this?
+
+#prelimnary linear model to see general contributory impact of other variables
+# Note that at the moment
+full.data$Term.2 <- ifelse(full.data$Term == "Terminated", c(1), c(0))
+
+summary(lm(Term.2 ~ Who + What + Intl + PSR + FxCb + Fee + Dur + SingleOffer + Soft +
+             UCA + CRai + Veh + UnmodifiedNumberOfOffersReceived, data = full.data))
+#logit model
+summary(glm(Term ~ Who + What + Intl + PSR + FxCb + Fee + Dur + SingleOffer + Soft +
+              UCA + CRai + Veh + UnmodifiedNumberOfOffersReceived, family = "binomial"(link = logit), data = full.data))
+
+####
+#### Model Training Section ####
+####
+
+#Train, Test, Validate partition
+set.seed(100)
+sample_train <- floor(.7*nrow(complete.data))
+sample_valid <- floor(.15*nrow(complete.data))
+
+index_train <- sort(sample(seq_len(nrow(complete.data)), size=sample_train))
+index_not.train <- setdiff(seq_len(nrow(complete.data)), index_train)
+index_valid  <- sort(sample(index_not.train, size=sample_valid))
+index_test  <- setdiff(index_not.train, index_valid)
+
+complete_train  <- complete.data[index_train, ]
+complete_valid <- complete.data[index_valid, ]
+complete_test <- complete.data[index_test, ]
+
+
+
+#set number of folds to be used in k-folds analysis
+j <- 10
+
+#Partition training data into 10 folds to train the model for
+complete.data$folds <- kfolds.index(nrow(complete.data), j)
+
+#Run kfolds, cycling through each k has the holdout
+set.seed(548)
+for(k in unique(complete.data$folds)){
+  
+  #Cut train/test: k is the holdhout
+  this.train <- complete.data[complete.data$folds != k, ]
+  this.test <- complete.data[complete.data$folds == k, ]
+  
+  #estimate model, using a random forest with 200 trees (no notable benefit from 500), and mtry = 3
+  #...as previous determined from the tuneRF function (commented out here)
+  #tuneRF(x = this.train[,c(4,8:18)], y = this.train$Term, step = 1.5, trace = TRUE)
+  this.rf <- randomForest(Term ~  Who + What + Intl + PSR + FxCb + Fee + Dur + SingleOffer + Soft +
+                            UCA + CRai + Veh + UnmodifiedNumberOfOffersReceived, data = this.train,
+                          mtry = 3, ntree = 200, na.action = na.omit, 
+                          #importance = TRUE, #uncomment this line to determine variable importance
+                          type = "classification")
+  
+  #Commented out here: report out the relative importance of each variable
+  #Useful to determine which variables can be dropped (as all of the point estimates were here)
+  #round(importance(full.rf), 2)
+  
+  
+  #Use model estimate to predict the holdout values for this k
+  this.pred <- pred.check(this.test, this.rf)
+  
+  #Print the meanf1 accuracy measure for this iteration of the model
+  print(paste0("meanF1 (holdout group ", k,") = ", this.pred))
+  
+}
+
+
+
+
+######Outside of loop test
+#set seed
+set.seed(123)
+this.rf <- randomForest(Term ~  Who + What + Intl + PSR + FxCb + Fee + Dur + Soft +
+                          UCA + CRai + Veh + UnmodifiedNumberOfOffersReceived, data = complete_train, #note using train data
+                        mtry = 4, ntree = 200, 
+                        #omit missing values - or should we impute (using rfImpute)?
+                        na.action = na.omit,
+                        #importance = TRUE, #uncomment this line to determine variable importance
+                        type = "classification")
+
+#determine which variables to remove (i.e. has too little influence)
+varImpPlot(this.rf)
+
+#find optimal mtry (should be 4)
+tuneRF(complete.data[,c("Who", "What","Intl", "PSR", "FxCb", "Fee", "Dur", 
+                        "Soft", "UCA", "CRai", "Veh", "UnmodifiedNumberOfOffersReceived")], complete.data$Term, ntreeTry = 400, na.action = na.omit, 
+       mtryStart = 1, stepFactor = 2, 
+       improve = 0.001, trace = TRUE, plot = TRUE)
+
+#checking predition
+pred.check(complete_train, this.rf)
+pred.check(complete_valid, this.rf)
+pred.check(complete_test, this.rf)
